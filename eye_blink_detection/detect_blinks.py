5bf200066ace9c207d6d73bbbcce47c98cf735c3
zhou <1536708889@qq.com>
2024-05-23 23:15:52 -0300

    fix

--- eye_blink_detection/detect_blinks.py
+++ eye_blink_detection/detect_blinks.py
@@ -5,7 +5,7 @@ import os.path
 
 # import the necessary packages
 from scipy.spatial import distance as dist
-from imutils.video import FileVideoStream
+from imutils.video import FileVideoStream, VideoStream
 from imutils import face_utils
 import argparse
 import imutils
@@ -30,133 +30,136 @@ def eye_aspect_ratio(eye):
     # return the eye aspect ratio
     return ear
 
+if __name__ == '__main__':
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-                help="path to facial landmark predictor")
-ap.add_argument("-v", "--video", type=str, default="",
-                help="path to input video file")
-ap.add_argument("-o", "--output_dir", required=False, default="out")
-args = vars(ap.parse_args())
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--shape_predicytor", required=False,
+                    default="shape_predictor_68_face_landmarks.dat",
+                    help="path to facial landmark predictor")
+    ap.add_argument("-v", "--video", type=str,
+                    default="blink_detection_demo.mp4",
+                    help="path to input video file")
+    ap.add_argument("-o", "--output_dir", required=False, default="output_dir")
+    args = vars(ap.parse_args())
 
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold
-EYE_AR_THRESH = 0.27
-EYE_AR_CONSEC_FRAMES = 3
+    # define two constants, one for the eye aspect ratio to indicate
+    # blink and then a second constant for the number of consecutive
+    # frames the eye must be below the threshold
+    EYE_AR_THRESH = 0.27
+    EYE_AR_CONSEC_FRAMES = 3
 
-# initialize the frame counters and the total number of blinks
-COUNTER = 0
-TOTAL = 0
+    # initialize the frame counters and the total number of blinks
+    COUNTER = 0
+    TOTAL = 0
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+    # initialize dlib's face detector (HOG-based) and then create
+    # the facial landmark predictor
+    print("[INFO] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(args["shape_predicytor"])
 
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    # grab the indexes of the facial landmarks for the left and
+    # right eye, respectively
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-# fileStream = True
-video_path = ""
-if not os.path.isabs(args["video"]):
-    video_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), args["video"]))
-vs = FileVideoStream(video_path).start()
+    # start the video stream thread
+    print("[INFO] starting video stream thread...")
+    # fileStream = True
+    video_path = ""
+    if not os.path.isabs(args["video"]):
+        video_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), args["video"]))
+    #vs = FileVideoStream(video_path).start()
+    vs = VideoStream(src=0).start()
+    output_dir = ""
+    if not os.path.isabs(args["output_dir"]):
+        output_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), args["output_dir"]))
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+    # vs = VideoStream(usePiCamera=True).start()
+    fileStream = True
+    time.sleep(1.0)
+    frame_cnt = 0
+    # loop over frames from the video stream
+    while True:
+        # if this is a file video stream, then we need to check if
+        # there any more frames left in the buffer to process
+        # if fileStream and not vs.more():
+        #     break
 
-output_dir = ""
-if not os.path.isabs(args["output_dir"]):
-    output_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), args["output_dir"]))
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-# vs = VideoStream(usePiCamera=True).start()
-fileStream = True
-time.sleep(1.0)
-frame_cnt = 0
-# loop over frames from the video stream
-while True:
-    # if this is a file video stream, then we need to check if
-    # there any more frames left in the buffer to process
-    if fileStream and not vs.more():
-        break
+        # grab the frame from the threaded video file stream, resize
+        # it, and convert it to grayscale
+        # channels)
+        frame = vs.read()
+        if frame is None:
+            break
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # grab the frame from the threaded video file stream, resize
-    # it, and convert it to grayscale
-    # channels)
-    frame = vs.read()
-    if frame is None:
-        break
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect faces in the grayscale frame
+        rects = detector(gray, 0)
 
-    # detect faces in the grayscale frame
-    rects = detector(gray, 0)
+        # loop over the face detections
+        for rect in rects:
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
 
-    # loop over the face detections
-    for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+            # extract the left and right eye coordinates, then use the
+            # coordinates to compute the eye aspect ratio for both eyes
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
 
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
+            # average the eye aspect ratio together for both eyes
+            ear = (leftEAR + rightEAR) / 2.0
 
-        # average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
+            # compute the convex hull for the left and right eye, then
+            # visualize each of the eyes
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-        # compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+            # check to see if the eye aspect ratio is below the blink
+            # threshold, and if so, increment the blink frame counter
+            if ear < EYE_AR_THRESH:
+                COUNTER += 1
 
-        # check to see if the eye aspect ratio is below the blink
-        # threshold, and if so, increment the blink frame counter
-        if ear < EYE_AR_THRESH:
-            COUNTER += 1
+            # otherwise, the eye aspect ratio is not below the blink
+            # threshold
+            else:
+                # if the eyes were closed for a sufficient number of
+                # then increment the total number of blinks
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    TOTAL += 1
 
-        # otherwise, the eye aspect ratio is not below the blink
-        # threshold
-        else:
-            # if the eyes were closed for a sufficient number of
-            # then increment the total number of blinks
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                TOTAL += 1
+                # reset the eye frame counter
+                COUNTER = 0
 
-            # reset the eye frame counter
-            COUNTER = 0
+            # draw the total number of blinks on the frame along with
+            # the computed eye aspect ratio for the frame
+            cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-        # draw the total number of blinks on the frame along with
-        # the computed eye aspect ratio for the frame
-        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        # show the frame
+        cv2.imshow("Frame", frame)
+        #output_file_name = "output_frame_" + str(frame_cnt) + ".png"
+        #output_image_path = os.path.join(output_dir, output_file_name)
+        #cv2.imwrite(f'{output_dir}/frame_{frame_cnt}.jpg', frame)
+        key = cv2.waitKey(1) & 0xFF
 
-    # show the frame
-    cv2.imshow("Frame", frame)
-    output_file_name = "output_frame_" + str(frame_cnt) + ".png"
-    output_image_path = os.path.join(output_dir, output_file_name)
-    cv2.imwrite(f'{output_dir}/frame_{frame_cnt}.jpg', frame)
-    key = cv2.waitKey(1) & 0xFF
+        frame_cnt+=1
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 
-    frame_cnt+=1
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
