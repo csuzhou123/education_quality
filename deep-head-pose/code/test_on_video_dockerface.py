e2e9c64ab960d7ed6eecd7df784ea6687f900172
NJUsw <131757393+NJUsw@users.noreply.github.com>
2024-05-27 16:04:31 +0800

    Add files via upload

---
+++ deep-head-pose/code/test_on_video_dockerface.py
@@ -0,0 +1,127 @@
+import sys, os, argparse
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.backends.cudnn as cudnn
+import torchvision
+import torch.nn.functional as F
+from PIL import Image
+import dlib
+import datasets, hopenet, utils
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Head pose estimation using the Hopenet network.')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]', default=0, type=int)
+    parser.add_argument('--snapshot', dest='snapshot', help='Path of model snapshot.', default='', type=str)
+    parser.add_argument('--video', dest='video_path', help='Path of video')
+    parser.add_argument('--bboxes', dest='bboxes', help='Bounding box annotations of frames', default='', type=str)
+    parser.add_argument('--output_string', dest='output_string', help='String appended to output file')
+    parser.add_argument('--n_frames', dest='n_frames', help='Number of frames', type=int)
+    parser.add_argument('--fps', dest='fps', help='Frames per second of source video', type=float, default=30.)
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    cudnn.enabled = True
+
+    batch_size = 1
+    gpu = args.gpu_id
+    snapshot_path = args.snapshot
+    out_dir = 'output/video'
+    video_path = args.video_path
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    if not os.path.exists(args.video_path):
+        sys.exit('Video does not exist')
+
+    # ResNet50 structure
+    model = hopenet.Hopenet(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 66)
+
+    print('Loading snapshot.')
+    # Load snapshot
+    saved_state_dict = torch.load(snapshot_path, map_location=torch.device('cpu'))
+    model.load_state_dict(saved_state_dict)
+
+    print('Loading data.')
+
+    transformations = transforms.Compose([transforms.Resize(224),
+                                          transforms.CenterCrop(224), transforms.ToTensor(),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    print('Ready to test network.')
+
+    # Test the Model
+    model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+    total = 0
+
+    idx_tensor = [idx for idx in range(66)]
+    idx_tensor = torch.FloatTensor(idx_tensor)
+
+    video = cv2.VideoCapture(video_path)
+
+    # New cv2
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))   # float
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)) # float
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter('output/video/output-%s.avi' % args.output_string, fourcc, args.fps, (width, height))
+
+    txt_out = open('output/video/output-%s.txt' % args.output_string, 'w')
+
+    frame_num = 1
+    detector = dlib.get_frontal_face_detector()
+
+    while frame_num <= args.n_frames:
+        ret, frame = video.read()
+        if not ret:
+            break
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+
+        for face in faces:
+            x_min, y_min, x_max, y_max = face.left(), face.top(), face.right(), face.bottom()
+            x_min = max(x_min, 0)
+            y_min = max(y_min, 0)
+            x_max = min(frame.shape[1], x_max)
+            y_max = min(frame.shape[0], y_max)
+            
+            # Crop image
+            img = frame[y_min:y_max, x_min:x_max]
+            img = Image.fromarray(img)
+
+            # Transform
+            img = transformations(img)
+            img = img.view(1, img.size(0), img.size(1), img.size(2))
+            img = Variable(img)
+
+            yaw, pitch, roll = model(img)
+
+            yaw_predicted = F.softmax(yaw, dim=1)
+            pitch_predicted = F.softmax(pitch, dim=1)
+            roll_predicted = F.softmax(roll, dim=1)
+            # Get continuous predictions in degrees.
+            yaw_predicted = torch.sum(yaw_predicted.data[0] * idx_tensor) * 3 - 99
+            pitch_predicted = torch.sum(pitch_predicted.data[0] * idx_tensor) * 3 - 99
+            roll_predicted = torch.sum(roll_predicted.data[0] * idx_tensor) * 3 - 99
+
+            txt_out.write(str(frame_num) + ' %f %f %f\n' % (yaw_predicted, pitch_predicted, roll_predicted))
+            utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx=(x_min + x_max) / 2, tdy=(y_min + y_max) / 2, size=(y_max - y_min) / 2)
+        
+        out.write(frame)
+        frame_num += 1
+
+    out.release()
+    video.release()
+    txt_out.close()
